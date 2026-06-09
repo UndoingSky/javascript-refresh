fc1777384845c93c75348260efcc2e5dc9a5b6a2
Aryan Gupta <lavigupta8200@gmail.com>
2026-06-09 20:57:17 +0530

    Learned variables and data types

--- day01/index.js
+++ day01/index.js
@@ -1,1 +1,23 @@
-console.log("JavaScript Refresh Day 1");
+
+
+let name = "Aryan";
+const age = 20;
+let isDeveloper = true;
+
+console.log(name);
+console.log(age);
+console.log(isDeveloper);
+
+
+
+let language = "JavaScript"; 
+let years = 20; 
+let learning = true; 
+let emptyValue = null; 
+let notDefined; 
+
+console.log(typeof language);
+console.log(typeof years);
+console.log(typeof learning);
+console.log(typeof emptyValue);
+console.log(typeof notDefined);
