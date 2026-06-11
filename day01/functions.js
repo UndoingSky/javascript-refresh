8d5ff461020b9dda5aa8437830718a23174a5976
Aryan Gupta <lavigupta8200@gmail.com>
2026-06-11 20:45:02 +0530

    Add initial functions for greeting, addition, and introduction

---
+++ day01/functions.js
@@ -0,0 +1,18 @@
+function greet(name) {
+    return `Hello, $(name)!`;
+}
+
+console.log(greet("Aryan"));
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(5, 10));
+
+function introduce(name, age) {
+    return `My name is ${name} and I am ${age} years old.`;
+}
+
+console.log(introduce("Aryan", 20));
+
